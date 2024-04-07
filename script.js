8e61be8999dd73dbb7201233d3f0c9f26cc2b0a8
radsadsoap <vinayaksh8432@gmail.com>
2024-04-07 20:45:10 +0530

    main

---
+++ script.js
@@ -0,0 +1,32 @@
+const button = document.querySelector(".button");
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+
+function getQuote() {
+   let api = `https://api.quotable.io/random?maxLength=45`;
+   fetch(api)
+      .then(function (response) {
+         return response.json();
+      })
+      .then(function (data) {
+         quote.content = data.content;
+         quote.author = data.author;
+      })
+      .then(function () {
+         displayQuote();
+      })
+      .catch(function (error) {
+         console.log(error);
+      });
+}
+
+function displayQuote() {
+   quote.innerHTML = `${quote.content}`;
+   author.innerHTML = `${quote.author}`;
+}
+
+button.addEventListener("click", function () {
+   getQuote();
+});
+
+getQuote();
